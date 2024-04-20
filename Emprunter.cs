7cdbe0f69fcfb4fd2a7b75da5f4cc5f22817cacd
Souhaib96 <156733944+Souhaib96@users.noreply.github.com>
2024-04-19 23:31:11 -0400

    Add files via upload

---
+++ Emprunter.cs
@@ -0,0 +1,10 @@
+﻿
+namespace ConsoleApp6
+{
+    public class Emprunter
+    {
+        public DateTime DateEmprunt { get; set; }
+        public DateTime DateRetour { get; set; }
+        public bool DocumentRetourne { get; set; }
+    }
+}
