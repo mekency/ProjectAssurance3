45ec052a7962d55012e0a48942d3b07b13fc5661
Yacine Toukal <yacinetoukal.tk@gmail.com>
2024-04-19 22:09:44 -0400

    first commit

---
+++ Utilisateur.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection.Metadata;
+
+namespace ConsoleApp6
+{
+    public class Utilisateur
+    {
+        public int ID { get; set; }
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public string Email { get; set; }
+        public string Adresse { get; set; }
+        public int Telephone { get; set; }
+
+        public void EmprunterDocument(Documents doc)
+        {
+            // Logique pour emprunter un document
+        }
+
+        public void RetournerDocument(Documents doc)
+        {
+            // Logique pour retourner un document
+        }
+    }
+
+}
