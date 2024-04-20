0d9bca44590e63f8750c967cd72cb2783beef4e6
Yacine Toukal <yacinetoukal.tk@gmail.com>
2024-04-19 21:58:23 -0400

    first commit

---
+++ Journal.cs
@@ -0,0 +1,16 @@
+﻿
+using System.Reflection.Metadata;
+
+namespace ConsoleApp6
+{
+    public class Journal : Documents
+    {
+        public DateTime DateParution { get; set; }
+
+        public string LireArticlePrincipal()
+        {
+            return "Ceci est l'article principal du journal.";
+        }
+    }
+
+}
