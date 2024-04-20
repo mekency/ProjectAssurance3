08028ecc2d2318430195dadf9dd827091b9bce8f
Souhaib96 <156733944+Souhaib96@users.noreply.github.com>
2024-04-19 22:53:19 -0400

    Add files via upload

---
+++ Documents.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp6
+{
+    public class Documents
+    {
+        public string ID { get; set; }
+        public string Titre { get; set; }
+        public int AnneePublication { get; set; }
+        public string Auteurs { get; set; }
+
+        public void AfficherDetails()
+        {
+            Console.WriteLine($"ID: {ID}, Titre: {Titre}, Année de publication: {AnneePublication}, Auteurs: {Auteurs}");
+        }
+    }
+
+}
---
+++ Livre.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection.Metadata;
+
+namespace ConsoleApp6
+{
+    public class Livre : Documents
+    {
+        public string ISBN { get; set; }
+        public string Editeur { get; set; }
+        public int NombrePages { get; set; }
+
+        public string LireExtrait()
+        {
+            return "Ceci est un extrait du livre.";
+        }
+    }
+
+}
